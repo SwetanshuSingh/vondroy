882511043f96d6cafdd4492e0fd7b692dd195d92
Swetanshu <swetanshusingh17@gmail.com>
2024-02-12 10:16:36 +0530

    Update : login form types

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,14 +1,88 @@
-import InputField from "./ui/InputField";
+import { ChangeEvent, useState } from "react";
 
-const LoginForm = () : React.JSX.Element => {
-    return (
-        <form action="" className="flex flex-col gap-8">
-            <InputField fieldValue="username" fieldType="text" placeholder="Enter your username" />
-            <InputField fieldValue="email" fieldType="email" placeholder="Enter your email" />
-            <InputField fieldValue="password" fieldType="password" placeholder="Enter your password" />
-            <button className="bg-[#2b2b2b] text-white py-3 w-[400px] rounded-xl capitalize font-semibold tracking-widest">Sign in</button>
-        </form>
-    )
-}
+const LoginForm = (): React.JSX.Element => {
+  const [formData, setFormData] = useState<{
+    username: string;
+    email: string;
+    password: string;
+  }>({ username: "", email: "", password: "" });
 
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => {
+      prev[evt.target.name] = evt.target.value;
+      return { ...prev };
+    });
+  };
+
+  return (
+    <form action="" className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="username"
+          className="text-[#2b2b2b] font-normal capitalize"
+        >
+          Username :
+        </label>
+
+        <input
+          id="username"
+          name="username"
+          type="text"
+          value={formData.username}
+          onChange={(evt) => {
+            handleChange(evt);
+          }}
+          placeholder="Enter your username"
+          className="w-[400px] h-[45px] border-2 border-gray-300 outline-none rounded-lg py-2 px-3 font-normal text-sm tracking-widest"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="email"
+          className="text-[#2b2b2b] font-normal capitalize"
+        >
+          Email :
+        </label>
+
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={(evt) => {
+            handleChange(evt);
+          }}
+          placeholder="Enter your email address"
+          className="w-[400px] h-[45px] border-2 border-gray-300 outline-none rounded-lg py-2 px-3 font-normal text-sm tracking-widest"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="password"
+          className="text-[#2b2b2b] font-normal capitalize"
+        >
+          Password :
+        </label>
+
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={(evt) => {
+            handleChange(evt);
+          }}
+          placeholder="Enter your password"
+          className="w-[400px] h-[45px] border-2 border-gray-300 outline-none rounded-lg py-2 px-3 font-normal text-sm tracking-widest"
+        />
+      </div>
+
+      <button className="bg-[#2b2b2b] text-white py-3 w-[400px] rounded-xl capitalize font-semibold tracking-widest">
+        Sign in
+      </button>
+    </form>
+  );
+};
 export default LoginForm;
--- client/src/components/ui/InputField.tsx
+++ client/src/components/ui/InputField.tsx
@@ -1,10 +1,21 @@
+import { ChangeEvent } from "react";
+
 type InputFieldProps = {
     fieldValue : string,
     placeholder : string,
-    fieldType : string 
+    fieldType : string
+    inputValue : {username: string; email: string; password: string; [key : string] : string}
+    setInputValue : React.Dispatch<React.SetStateAction<{username : string, email : string, password : string}>>
 }
 
-const InputField = ({ fieldValue, placeholder, fieldType } : InputFieldProps): React.JSX.Element => {
+const InputField = ({ fieldValue, placeholder, fieldType, inputValue, setInputValue } : InputFieldProps): React.JSX.Element => {
+
+  const handleChange = (evt : ChangeEvent<HTMLInputElement>){
+    setInputValue((prev) => {
+      return ({...prev,evt.target.name = evt.target.value})
+    })
+  }
+
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={fieldValue} className="text-[#2b2b2b] font-normal capitalize">
@@ -15,6 +26,8 @@ const InputField = ({ fieldValue, placeholder, fieldType } : InputFieldProps): R
         id={fieldValue}
         name={fieldValue}
         type={fieldType}
+        value={inputValue[fieldValue]}
+        // onChange={handleChange}
         placeholder={placeholder}
         className="w-[400px] h-[45px] border-2 border-gray-300 outline-none rounded-lg py-2 px-3 font-normal text-sm tracking-widest"
       />
