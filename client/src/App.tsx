602469ba2c3a01dd3869718f48cbd9c5693bf9fa
Swetanshu <swetanshusingh17@gmail.com>
2024-02-19 22:29:14 +0530

    Add : user card and dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 const App = (): React.JSX.Element => {
   return (
@@ -10,6 +11,7 @@ const App = (): React.JSX.Element => {
           <Route path="/" element = {<HomePage />} />
           <Route path = "/signup" element = {<SignupPage />} />
           <Route path = "/login" element = {<LoginPage />} />
+          <Route path="/chat" element = {<ChatPage />} />
       </Routes>
     </BrowserRouter>
   );
