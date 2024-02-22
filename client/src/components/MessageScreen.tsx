0536dcba0529b3a29621d5068485c428284ebea3
Swetanshu <swetanshusingh17@gmail.com>
2024-02-22 09:26:32 +0530

    update : minor fixes

--- client/src/components/MessageScreen.tsx
+++ client/src/components/MessageScreen.tsx
@@ -1,21 +1,40 @@
+import { useContext, useEffect, useRef } from "react";
 import useGetMessages from "../hooks/useGetmessages";
+import { AuthContext } from "../context/AuthContext";
+import { ConversationContext } from "../context/ConversationContext";
 
 const MessageScreen = (): React.JSX.Element => {
+
   const { messages } = useGetMessages();
+  const {  auth } = useContext(AuthContext);
+  const { selectedConversation } = useContext(ConversationContext);
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({behavior : "smooth"})
+    }, 100);
+  }, [messages])
+  
   const conversationMessages = messages?.messages;
 
   return (
     <div className="messages flex flex-col gap-3 flex-grow p-3 overflow-y-scroll">
       {conversationMessages ? (
         conversationMessages.map((message) => {
+          const fromMe = auth.id === message.senderId
+          const chatClassname = fromMe ? 'flex-row-reverse' : 'flex-row';
+          const borderClassname = fromMe ? 'rounded-l-md rounded-br-md' : 'rounded-r-md rounded-bl-md';
+          const profilePic = fromMe ? auth.profile : selectedConversation.profilePic;
+
           return (
-            <div className="message flex justify-start gap-2">
+            <div key={message.id} ref={lastMessageRef} className={`${chatClassname} message flex justify-start gap-2`}>
             <img
               className="w-8 h-8 border border-gray-400 rounded-full box-border"
-              src="https://avatar.iran.liara.run/public/boy?username=Swetanshu"
+              src={profilePic}
               alt="avatar"
             />
-            <div className="bg-gray-100 mt-2 w-fit max-w-[70%] h-auto text-sm p-2 flex justify-center items-center flex-wrap rounded-r-md rounded-bl-md">
+            <div className={`${borderClassname} bg-gray-100 mt-2 w-fit max-w-[70%] h-auto text-sm p-2 flex justify-center items-center flex-wrap`}>
               {message.body}
             </div>
           </div>
