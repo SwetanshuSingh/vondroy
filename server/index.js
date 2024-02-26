96fed93360aa46f93d1c27423f6ca13c8ea18ad0
Swetanshu <swetanshusingh17@gmail.com>
2024-02-26 09:48:12 +0530

    Add : final deployment code

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,6 +11,8 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 // app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
@@ -17,6 +20,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,"client", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
   console.log("Server running");
